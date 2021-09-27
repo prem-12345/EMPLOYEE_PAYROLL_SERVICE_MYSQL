6a374abc1b16ef162fbbdac72107b440580ae51f
prem-12345 <premkpatil1997@gmail.com>
2021-09-27 17:16:56 +0530

    Added query to create employee payroll data

--- src/main/java/com/bridgelabz/employeepayrollservicemysql/EmployeePayrollServiceMysql.sql
+++ src/main/java/com/bridgelabz/employeepayrollservicemysql/EmployeePayrollServiceMysql.sql
@@ -6,7 +6,10 @@
  use employee_payroll_service;
  select database;
 
- # "UC2 - CREATE TABLE EMPLOYEE PAYROLL "
+ # "UC2 - CREATE TABLE EMPLOYEE PAYROLL"
  use employee_payroll_service;
  create table employee_payroll(ID int NOT NULL AUTO_INCREMENT UNIQUE,NAME varchar(150) NOT NULL,SALARY double NOT NULL,START date NOT NULL);
  describe employee_payroll;
+
+ # "UC3 - CREATE EMPLOYEE PAYROLL DATA"
+ insert into employee_payroll(ID,NAME,SALARY,START)values(1,'PREM',10000.00,'2018-06-01'),(2,'KALPESH',20000.00,'2019-06-01'),(3,'RAHUL',15000.00,'2017-06-01');

9694af0b4aee065b732d507045a31173b339a505
prem-12345 <premkpatil1997@gmail.com>
2021-09-27 17:05:29 +0530

    Added query to create employee payroll service database

--- src/main/java/com/bridgelabz/employeepayrollservicemysql/EmployeePayrollServiceMysql.sql
+++ src/main/java/com/bridgelabz/employeepayrollservicemysql/EmployeePayrollServiceMysql.sql
@@ -1,3 +1,7 @@
 # "WELCOME TO EMPLOYEE PAYROLL SERVICE MYSQL"
 
-
+ # "UC1 - CREATE EMPLOYEE PAYROLL SERVICE DATABASE"
+ create database employee_payroll_service;
+ show databases;
+ use employee_payroll_service;
+ select database;
